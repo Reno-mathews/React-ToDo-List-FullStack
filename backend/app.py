375cd880a09d83105194bde8beba9b1cfb18e375
Reno-mathews <renomathews10pro@gmail.com>
2025-12-31 15:43:17 +0530

    Update

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = \
     "postgresql://postgres:Halosucks10%40@localhost:5432/todo_app"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
 
 tasks = []
 task_id = 1
